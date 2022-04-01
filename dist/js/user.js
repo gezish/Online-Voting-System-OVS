3e02133733e686a33f50c0a0c9a4cd792eb7b475
gezish <enggezahegn.w@gmail.com>
2022-04-01 09:44:16 +0300

    cont

---
+++ dist/js/user.js
@@ -0,0 +1,273 @@
+//function to handle login-form validation
+function loginValidate(loginForm){
+
+var validationVerified=true;
+var errorMessage="";
+var okayMessage="click OK to continue";
+
+if (loginForm.voters_id.value=="")
+{
+errorMessage+="voters_id not filled!\n";
+validationVerified=false;
+}
+if(loginForm.password.value=="")
+{
+errorMessage+="Password not filled!\n";
+validationVerified=false;
+}
+if (!isValidvoters_id(loginForm.voters_id.value)) {
+errorMessage+="Invalid voters_id address provided!\n";
+validationVerified=false;
+}
+if(!validationVerified)
+{
+alert(errorMessage);
+}
+if(validationVerified)
+{
+alert(okayMessage);
+}
+return validationVerified;
+}
+
+//function to handle register-form validation
+function registerValidate(registerForm){
+
+var validationVerified=true;
+var errorMessage="";
+var okayMessage="click OK to process registration";
+
+if (registerForm.firstname.value=="")
+{
+errorMessage+="Firstname not filled!\n";
+validationVerified=false;
+}
+if (registerForm.middlename.value=="")
+{
+errorMessage+="middlename not filled!\n";
+validationVerified=false;
+}
+if(registerForm.lastname.value=="")
+{
+errorMessage+="Lastname not filled!\n";
+validationVerified=false;
+}
+if (registerForm.voters_id.value=="")
+{
+errorMessage+="voters_id not filled!\n";
+validationVerified=false;
+}
+if (registerForm.Gender.value=="")
+{
+errorMessage+="Gender not filled!\n";
+validationVerified=false;
+}
+if (registerForm.date.value=="")
+{
+errorMessage+="date not filled!\n";
+}
+if (registerForm.sex.value=="")
+{
+errorMessage+="sex not filled!\n";
+}
+validationVerified=false;
+}
+if(registerForm.password.value=="")
+{
+errorMessage+="Password not provided!\n";
+validationVerified=false;
+}
+if(registerForm.ConfirmPassword.value=="")
+{
+errorMessage+="Confirm password not filled!\n";
+validationVerified=false;
+}
+if(registerForm.ConfirmPassword.value!=registerForm.password.value)
+{
+errorMessage+="Confirm password and password do not match!\n";
+validationVerified=false;
+}
+if (!isValidEmail(registerForm.email.value)) {
+errorMessage+="Invalid email address provided!\n";
+validationVerified=false;
+}
+if(!validationVerified)
+{
+alert(errorMessage);
+}
+if(validationVerified)
+{
+alert(okayMessage);
+}
+return validationVerified;
+}
+
+//function to handle update-form validation
+function updateProfile(registerForm){
+
+var validationVerified=true;
+var errorMessage="";
+var okayMessage="click OK to update your account";
+
+if (registerForm.firstname.value=="")
+{
+errorMessage+="Firstname not filled!\n";
+validationVerified=false;
+}
+if(registerForm.lastname.value=="")
+{
+errorMessage+="Lastname not filled!\n";
+validationVerified=false;
+}
+if (registerForm.email.value=="")
+{
+errorMessage+="Email not filled!\n";
+validationVerified=false;
+}
+if(registerForm.password.value=="")
+{
+errorMessage+="New password not provided!\n";
+validationVerified=false;
+}
+if(registerForm.ConfirmPassword.value=="")
+{
+errorMessage+="Confirm password not filled!\n";
+validationVerified=false;
+}
+if(registerForm.ConfirmPassword.value!=registerForm.password.value)
+{
+errorMessage+="Confirm password and new password do not match!\n";
+validationVerified=false;
+}
+if (!isValidEmail(registerForm.email.value)) {
+errorMessage+="Invalid email address provided!\n";
+validationVerified=false;
+}
+if(!validationVerified)
+{
+alert(errorMessage);
+}
+if(validationVerified)
+{
+alert(okayMessage);
+}
+return validationVerified;
+}
+
+//validate email function
+function isValidvoters_id(val) {
+	var re = /^[\w\+\'\.-];
+	if (!re.test(val)) {
+		return false;
+	}
+    return true;
+}
+
+//validate special PIN
+function isValidSpecialPIN(val) {
+	var re = /^[0-9][0-9][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/;
+	if (!re.test(val)) {
+		return false;
+	}
+	return true;
+}
+
+//validate special PIN length
+function isValidLength(val){
+	var length = 12;
+	if (!re.test(val)) {
+		return false;
+	}
+	return true;
+}
+
+// onchange of qty field entry totals the price
+function getProductTotal(field) {
+    clearErrorInfo();
+    var form = field.form;
+	if (field.value == "") field.value = 0;
+	if ( !isPosInt(field.value) ) {
+        var msg = 'Please enter a positive integer for quantity.';
+        addValidationMessage(msg);
+        addValidationField(field)
+        displayErrorInfo( form );
+        return;
+	} else {
+		var product = field.name.slice(0, field.name.lastIndexOf("_") ); 
+        var price = form.elements[product + "_price"].value;
+		var amt = field.value * price;
+		form.elements[product + "_tot"].value = formatDecimal(amt);
+		doTotals(form);
+	}
+}
+
+
+//validate orderform
+function finalCheck(orderForm) {
+    var validationVerified=true;
+var errorMessage="";
+var okayMessage="click OK to process your order";
+
+if (orderForm.quantity.value=="")
+{
+errorMessage+="Please provide a quantity.\n";
+validationVerified=false;
+}
+if (orderForm.quantity.value==0)
+{
+errorMessage+="Please provide a quantity rather than 0.\n";
+validationVerified=false;
+}
+if(orderForm.total.value=="")
+{
+errorMessage+="Total has not been calculated! Please provide first the quantity.\n";
+validationVerified=false;
+}
+if(!validationVerified)
+{
+alert(errorMessage);
+}
+if(validationVerified)
+{
+alert(okayMessage);
+}
+return validationVerified;
+}
+
+//validate updateForm
+function updateValidate(updateForm) {
+    var validationVerified=true;
+var errorMessage="";
+var okayMessage="click OK to change your password";
+
+if (updateForm.opassword.value=="")
+{
+errorMessage+="Please provide your old password.\n";
+validationVerified=false;
+}
+if (updateForm.npassword.value=="")
+{
+errorMessage+="Please provide a new password.\n";
+validationVerified=false;
+}
+if(updateForm.cpassword.value=="")
+{
+errorMessage+="Please confirm your new password.\n";
+validationVerified=false;
+}
+if(updateForm.cpassword.value!=updateForm.npassword.value)
+{
+errorMessage+="Confirm password and new password do not match!\n";
+validationVerified=false;
+}
+if(!validationVerified)
+{
+alert(errorMessage);
+}
+if(validationVerified)
+{
+alert(okayMessage);
+}
+return validationVerified;
+}
+
