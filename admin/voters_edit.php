ea3017499b9a3546777da00160a45fcff441371c
gezish <enggezahegn.w@gmail.com>
2022-04-01 15:08:26 +0300

    css

---
+++ admin/voters_edit.php
@@ -0,0 +1,39 @@
+<?php
+	include 'includes/session.php';
+
+	if(isset($_POST['edit'])){
+		$id = $_POST['id'];
+		$firstname = $_POST['firstname'];
+		$middlename = $_POST['middlename'];
+		$lastname = $_POST['lastname'];
+		$Voters_id = $_POST['Voters_id'];
+		$sex = $_POST['sex'];
+		$date = $_POST['date'];
+		$password = $_POST['password'];
+
+		$sql = "SELECT * FROM voters WHERE id = $id";
+		$query = $conn->query($sql);
+		$row = $query->fetch_assoc();
+
+		if($password == $row['password']){
+			$password = $row['password'];
+		}
+		else{
+			$password = password_hash($password, PASSWORD_DEFAULT);
+		}
+
+		$sql = "UPDATE voters SET firstname = '$firstname', middlename = '$middlename', lastname = '$lastname', Voters_id='$Voters_id', sex = '$sex', date = '$date', password = '$password' WHERE id = '$id'";
+		if($conn->query($sql)){
+			$_SESSION['success'] = 'Voter updated successfully';
+		}
+		else{
+			$_SESSION['error'] = $conn->error;
+		}
+	}
+	else{
+		$_SESSION['error'] = 'Fill up edit form first';
+	}
+
+	header('location: voters.php');
+
+?>
