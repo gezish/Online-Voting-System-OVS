810f3dd7391d25f5c678ba5f04eaae661d4bde64
gezish <enggezahegn.w@gmail.com>
2022-04-01 12:38:20 +0300

    css

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>online Voting System</b>
+    </div>
+    <strong> &copy; 2021 Brought To You By Admas University PG Students</strong>
+</footer>
